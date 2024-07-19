689e6b409c4b8654ba296fe36591621dd50a174c
Marcy <87911841+DemonBunnyBon@users.noreply.github.com>
2024-07-19 20:23:58 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,9 +9,9 @@ using BowRepairMod;
 [assembly: AssemblyCopyright("Marcy")]
 
 
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: MelonInfo(typeof(BowRepair), "Bow Repair", "1.1.0", "Marcy", null)]
+[assembly: AssemblyVersion("1.1.1")]
+[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: MelonInfo(typeof(BowRepair), "BowRepair", "1.1.1", "Marcy", null)]
 
 
 [assembly: MelonGame("Hinterland", "TheLongDark")]
